15231cac4b3113b221dbfc226d19b5188ceb2a95
amahadnezhad <amahadnezhad@gmail.com>
2025-03-02 00:56:19 +0330

    Connect Bootstrap To _base.html Template

--- counter/views.py
+++ counter/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def word_counter(request):
-    return HttpResponse("hi")
+    return render(request, 'counter/counter.html')
